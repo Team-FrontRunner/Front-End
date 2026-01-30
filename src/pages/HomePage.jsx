38847a2f6ea050f3dc94076a41410cc0b8138f9a
wonyeoe <yeowonlee04@gmail.com>
2026-01-30 21:29:18 +0900

    mystatPage 완료

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -18,7 +18,7 @@ export default function HomePage() {
           오늘 몸은<br />
           어떠신가요?
         </h1>
-        <button className="health-check-btn">
+        <button className="health-check-btn" onClick={() => navigate('/mystat')}>
           <div className="health-icon">
             <img src={heartIcon} alt="heart" />
           </div>
